b229bde1b498a144fa9df6b1a956ddccbef0fd65
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 21:40:43 +0530

    [Rutwik]: Added Password Validation File

--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -11,7 +11,7 @@ namespace RegexUserRegistration
             while (flag)
             {
                 Console.WriteLine("Welcome to the User Registration validation program");
-                Console.WriteLine("Enter the validation to be executed : \n 1. First Name \n 2. Last Name \n 3. Email ID \n 4. Mobile \n 5. Password Rule-1 \n 6. Exit");
+                Console.WriteLine("Enter the validation to be executed : \n 1. First Name \n 2. Last Name \n 3. Email ID \n 4. Mobile \n 5. Password Rule-1 \n 6. Password Rule-2 \n 7. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -31,6 +31,9 @@ namespace RegexUserRegistration
                         Console.WriteLine(validation.ValidatePassword1("naveenve"));
                         break;
                     case 6:
+                        Console.WriteLine(validation.ValidatePassword2("navEenve"));
+                        break;
+                    case 7:
                         flag = false;
                         break;
                 }
--- RegexUserRegistration/UserRegistration.cs
+++ RegexUserRegistration/UserRegistration.cs
@@ -11,7 +11,8 @@ namespace RegexUserRegistration
         public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
         public static string REGEX_EMAIL = "^[a-z]{3,}[_+-.]{0,1}[a-z]{3,}[@][a-z]{2,}[.][a-z]{2}[.][a-z]{2}";
         public static string REGEX_MOBILE = "^[1,9]{2} [6-9]{1}[0-9]{9}";
-        public static string REGEX_PASSWORD1 = "^[a-z]{8}";
+        public static string REGEX_PASSWORD1 = "^[a-z]{8,}";
+        public static string REGEX_PASSWORD2 = "^[a-zA-Z]{8,}";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRST_NAME);
@@ -31,6 +32,10 @@ namespace RegexUserRegistration
         public bool ValidatePassword1(string password1)
         {
             return Regex.IsMatch(password1, REGEX_PASSWORD1);
+        }
+        public bool ValidatePassword2(string password2)
+        {
+            return Regex.IsMatch(password2, REGEX_PASSWORD2);
         }
     }
 }
