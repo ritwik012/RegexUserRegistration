f39b3427d4257a9e1bde9bd9a51039d0451d509e
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 21:30:28 +0530

    [Rutwik]: Added First Name File

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration validation = new UserRegistration();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to the User Registration validation program");
+                Console.WriteLine("Enter the validation to be executed : \n 1. First Name \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(validation.ValidateFirstName("Naveen"));
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RegexUserRegistration/UserRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    public class UserRegistration
+    {
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRST_NAME);
+        }
+    }
+}
